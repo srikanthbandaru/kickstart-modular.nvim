166818d27403dec110abe8868202e9c61f3f4711
Srikanth Bandaru <srikanthb.dev@gmail.com>
2025-03-24 11:53:29 -0500

    feat: keymap for yanking entire buffer content
    and setup mini.move

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -45,4 +45,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.keymap.set('n', '<C-a>', 'mm:%y<CR>`m', { noremap = true })
+
 -- vim: ts=2 sts=2 sw=2 et
--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -32,6 +32,7 @@ return {
         return '%2l:%-2v'
       end
 
+      require('mini.move').setup()
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
